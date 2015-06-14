ebb2587e7c0013cab25a50765cece65e07d3c95f
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-06-14 15:27:21 -0700

    cleaned up the console output

--- src/cli/main.go
+++ src/cli/main.go
@@ -3,23 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"wu"
 )
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) != 1 {
-		fmt.Println("Usage: wucli <location>")
+	var location string
+	if len(argsWithoutProg) == 0 {
+		location = "autoip"
+	} else if len(argsWithoutProg) == 1 {
+		location = argsWithoutProg[0]
+	} else {
+		fmt.Println("Usage: wucli [location]")
 		return
 	}
 
-	location := argsWithoutProg[0]
 	wd, err := wu.Query(location)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	locationResults := wd.Response.Results
+	if locationResults != nil {
+		fmt.Println("Locations matched:")
+		for _, lr := range locationResults {
+			if lr.State != "" {
+				fmt.Printf("  * %s, %s, %s (zmw:%s)\n", lr.City, lr.State, lr.CountryName, lr.Zmw)
+			} else {
+				fmt.Printf("  * %s, %s (zmw:%s)\n", lr.City, lr.CountryName, lr.Zmw)
+			}
+		}
+		return
+	}
+
 	current := wd.CurrentObservation
+	loc := current.DisplayLocation
+	forecast := wd.Forecast.TxtForecast
+
+	title := fmt.Sprintf("Location: %s (long: %s, lat: %s)", loc.Full, loc.Longitude, loc.Latitude)
+	fmt.Println(title)
+	fmt.Println(strings.Repeat("=", len(title)))
+
+	fmt.Printf("Summary: %s\n", current.Weather)
 	fmt.Printf("Temperature: %s\n", current.TemperatureString)
+	fmt.Printf("Feels like: %s\n", current.FeelslikeString)
+	fmt.Printf("Wind: %s\n", current.WindString)
+	fmt.Printf("Wind chill: %s\n", current.WindchillString)
+	fmt.Printf("Precipitation: %s\n", current.PrecipTodayString)
+
+	fmt.Println("Weather Forecast:")
+	for _, fc := range forecast.Forecastday {
+		fmt.Printf("  * %s: %s\n", fc.Title, fc.Fcttext)
+	}
+
+	fmt.Printf("\n%s\n", current.ObservationTime)
 }
